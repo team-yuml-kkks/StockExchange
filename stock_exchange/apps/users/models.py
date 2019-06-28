b963a2730f7eb1ea0c520d6b15791550615769c5
Kamil Supera <kamil.supera@makimo.pl>
2019-06-28 11:44:01 +0200

    STOCK-3 Add extra fields to user model and admin.

--- stock_exchange/apps/users/models.py
+++ stock_exchange/apps/users/models.py
@@ -3,4 +3,11 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    """
+    Extending User model of field with stored
+    extra information about user.
+    """
+    account_number = models.CharField(max_length=200, blank=True, null=True)
+    bank_name = models.CharField(max_length=100, blank=True, null=True)
+    address = models.CharField(max_length=300, blank=True, null=True)
+    city_code = models.CharField(max_length=7, blank=True, null=True)
--- stock_exchange/settings/base.py
+++ stock_exchange/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from django.conf import settings
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -102,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
